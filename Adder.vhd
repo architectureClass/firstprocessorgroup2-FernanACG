84a31a8fddbd05f627d9acdc54665786c39bc544
FernanACG <feralk12@utp.edu.co>
2017-04-05 14:48:20 -0500

    Update Adder.vhd

--- Adder.vhd
+++ Adder.vhd
@@ -3,11 +3,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity Add is
+entity Adder is
     Port ( Op1 : in  STD_LOGIC_VECTOR (31 downto 0);
            Op2 : in  STD_LOGIC_VECTOR (31 downto 0);
            Salida : out  STD_LOGIC_VECTOR (31 downto 0):= (others => '0'));
-end Add;
+end Adder;
 
 architecture Behavioral of Add is
 
