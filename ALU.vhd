46307e73922b2c0717c8d8076e7eb59dc09e6966
FernanACG <feralk12@utp.edu.co>
2017-04-05 17:13:08 -0500

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -14,27 +14,27 @@ architecture Behavioral of ALU is
 
 begin
 
-	process(operando1,operando2,aluOP)
-	begin
-	   case aluOP is 
-			when "000000" => -- add
-				AluResult <= operando1 + operando2;
-			when "000001" => -- sub
-				AluResult <= operando1 - operando2;
-			when "000010" => -- or
-				AluResult <= operando1 or operando2;
-			when "000011" => -- and
-				AluResult <= operando1 and operando2;
-			when "000100" => -- xor
-				AluResult <= operando1 xor operando2;
-			when "000101" => -- nor operando 1 or not operando2
-				AluResult <= operando1 nor operando2;
-			when "000110" => -- andn
-				AluResult <= operando1 nand operando2;
-			when "000111" => --xnor
-				AluResult <= operando1 xnor operando2;
-			when others => 
-				AluResult <= "00000000000000000000000000000000";
-		end case;
-	end process;
+process(operando1,operando2,aluOP)
+begin
+   case aluOP is 
+	when "000000" => -- add
+		AluResult <= operando1 + operando2;
+	when "000001" => -- sub
+		AluResult <= operando1 - operando2;
+	when "000010" => -- or
+		AluResult <= operando1 or operando2;
+	when "000011" => -- and
+		AluResult <= operando1 and operando2;
+	when "000100" => -- xor
+		AluResult <= operando1 xor operando2;
+	when "000101" => -- nor operando 1 or not operando2
+		AluResult <= operando1 nor operando2;
+	when "000110" => -- andn
+		AluResult <= operando1 nand operando2;
+	when "000111" => --xnor
+		AluResult <= operando1 xnor operando2;
+	when others => 
+		AluResult <= "00000000000000000000000000000000";
+	end case;
+end process;
 end Behavioral;
